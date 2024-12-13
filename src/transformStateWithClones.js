b793a702314a90032c914c667bdea4b7158449b4
diachkinainna <diachkinainna@gmail.com>
2024-12-13 15:58:15 +0200

    add switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,31 +11,27 @@ function transformStateWithClones(state, actions) {
   const clone = { ...state };
 
   for (const object of actions) {
-    if (object.type === 'addProperties') {
-      const { extraData } = object;
+    switch (object.type) {
+      case 'addProperties':
+        const { extraData } = object;
 
-      Object.assign(clone, extraData);
+        Object.assign(clone, extraData);
+        break;
 
-      stateHistory.push({ ...clone });
+      case 'removeProperties':
+        const { keysToRemove } = object;
+
+        for (const key of keysToRemove) {
+          delete clone[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in clone) {
+          delete clone[key];
+        }
     }
-
-    if (object.type === 'removeProperties') {
-      const { keysToRemove } = object;
-
-      for (const key of keysToRemove) {
-        delete clone[key];
-      }
-
-      stateHistory.push({ ...clone });
-    }
-
-    if (object.type === 'clear') {
-      for (const key in clone) {
-        delete clone[key];
-      }
-
-      stateHistory.push({});
-    }
+    stateHistory.push({ ...clone });
   }
 
   return stateHistory;
